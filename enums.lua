76a91d42a1e34a2fab065b5b81749f2e46bc0c23
randomdudeinternet <trdotrdo35@gmail.com>
2025-05-31 11:13:44 -0300

    Updated enums.lua
    updated so it has Vector3 and Part

--- enums.lua
+++ enums.lua
@@ -1,1 +1,23 @@
-
+Vector3 = {}
+Vector3.new = function (a,b,c)
+    return
+end
+Part = {}
+Part.ClassName = nil
+Part.Position = nil
+Part.GUID = nil
+function Part:GetColor()
+    return
+end
+function Part:GetSize()
+    return
+end
+function Part:Trigger()
+    return
+end
+function Part:Configure(table)
+    return
+end
+function Part:Connect(event,func)
+    return
+end
