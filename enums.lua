59f47d53c9610be94a754f29802aad0788903935
randomdudeinternet <trdotrdo35@gmail.com>
2025-05-31 11:25:57 -0300

    Update enums.lua
    Added Part_init
    Added Gyro
    Added Keyboard
    added Microphone

--- enums.lua
+++ enums.lua
@@ -2,22 +2,40 @@ Vector3 = {}
 Vector3.new = function (a,b,c)
     return
 end
-Part = {}
-Part.ClassName = nil
-Part.Position = nil
-Part.GUID = nil
-function Part:GetColor()
+local Part_init = function(Part)
+    Part.ClassName = nil
+    Part.Position = nil
+    Part.GUID = nil
+    function Part:GetColor()
+        return
+    end
+    function Part:GetSize()
+        return
+    end
+    function Part:Trigger()
+        return
+    end
+    function Part:Configure(table)
+        return
+    end
+    function Part:Connect(event,func)
+        return
+    end
+end
+local Part = {}
+Part_init(Part)
+local Gyro = {}
+Part_init(Gyro)
+function Gyro:PointAt(Vector3)
     return
 end
-function Part:GetSize()
+local Keyboard = {}
+Part_init(Keyboard)
+function Keyboard:SimulateKeyPress(key,Player)
     return
 end
-function Part:Trigger()
+function Keyboard:SimulateTextInput(input,Player)
     return
 end
-function Part:Configure(table)
-    return
-end
-function Part:Connect(event,func)
-    return
-end
+local Microphone = {}
+Part_init(Microphone)
